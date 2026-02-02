069804578e09626fc12d733994db8b29623f6656
013Ratchanon <dewtete@gmail.com>
2026-02-02 14:16:09 +0700

    text

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'node:url'
 import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
 import viteConfig from './vite.config'
-
+//
 export default mergeConfig(
   viteConfig,
   defineConfig({
